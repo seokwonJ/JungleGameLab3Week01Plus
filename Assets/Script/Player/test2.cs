47c1b8d9b161dc9a47450c69fbbc859f363eae3e
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2025-03-07 10:43:49 +0900

    충돌 테스트

--- Assets/Script/Player/test2.cs
+++ Assets/Script/Player/test2.cs
@@ -5,12 +5,13 @@ public class test2 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //fdsafdsasa
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //fdsafdsasa
+        //fafasdadsadasd
     }
 }
