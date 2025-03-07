c11b741636ba5dd6f333b4ddb0e6096016d5189d
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2025-03-07 11:21:53 +0900

    start 삭제

--- Assets/Script/Player/test2.cs
+++ Assets/Script/Player/test2.cs
@@ -2,9 +2,5 @@ using UnityEngine;
 
 public class test2 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void Start()
-    {
-        // git branch 111111
-    }
+
 }
