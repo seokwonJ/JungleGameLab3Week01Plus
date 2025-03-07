d74be12658f40868816ad6fa84390ac0c77908a1
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2025-03-07 10:39:46 +0900

    Clash Change

--- Assets/Script/Player/test2.cs
+++ Assets/Script/Player/test2.cs
@@ -3,14 +3,8 @@ using UnityEngine;
 public class test2 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //git branch Test2
     }
 }
